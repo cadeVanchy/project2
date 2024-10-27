c592e9443984efb141d918c45e10f49b0f0172ec
Cade LaVanchy <cadelavanchy@mines.edu>
2024-10-27 17:16:40 -0600

    final code

--- mkdir_splitpath.c
+++ mkdir_splitpath.c
@@ -4,16 +4,74 @@
 
 extern struct NODE* root;
 extern struct NODE* cwd;
+struct NODE* lastNode;
+char* path;
 
+struct NODE* nodeSearch(struct NODE* node, char* targetName, char* grandpaName){
+    if(node == NULL){
+        return NULL;
+    }
+    if(strcmp(node->name, targetName) == 0 && strcmp(node->parentPtr->name, grandpaName) == 0){
+        return node;
+    }
+    struct NODE* foundNode = nodeSearch(node->siblingPtr, targetName, grandpaName);
+    if(foundNode != NULL){
+        return foundNode;
+    }
+    return nodeSearch(node->childPtr, targetName, grandpaName);
+    
+}
 //make directory
 void mkdir(char pathName[]){
 
     // TO BE IMPLEMENTED
     //
     // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
-
-    printf("TO BE IMPLEMENTED\n");
-
+    path = malloc(100);
+    if(strcmp(pathName, "/") == 0){
+        printf("MKDIR ERROR: no path provided\n");
+        return;
+    }
+    char* baseName = malloc(100);
+    char* dirName = malloc(100); 
+    struct NODE* targetNode = splitPath(pathName, baseName, dirName);
+    if(targetNode == NULL){
+        char* token = strtok(path, "/");
+        struct NODE* checkNode = nodeSearch(root, token, "/");
+        while(token != NULL){
+            if(checkNode == NULL){
+                break;
+            }
+            token = strtok(NULL, "/");
+            checkNode = nodeSearch(root, token, checkNode->name);
+        }
+        printf("ERROR: Directory %s already exists\n", path);
+        return;
+    }
+    // check if target directory already in current directoy
+    if(nodeSearch(root, baseName, targetNode->name) != NULL){
+        printf("ERROR: Directory %s already exists\n", path);
+        return;
+    }
+    struct NODE* newNode = (struct NODE*)malloc(sizeof(struct NODE));
+    newNode->fileType = 'D';
+    newNode->childPtr = NULL;
+    newNode->siblingPtr = NULL;
+    newNode->parentPtr = targetNode;
+    strcpy(newNode->name, baseName);
+    if(targetNode->childPtr == NULL){
+        targetNode->childPtr = newNode;
+    } else {
+        lastNode = targetNode->childPtr;
+        while(1){
+            if(lastNode->siblingPtr == NULL) {
+                lastNode->siblingPtr = newNode;
+                break;
+            }
+            lastNode = lastNode->siblingPtr;
+        }
+    }
+    printf("MKDIR SUCESS: node %s successfully created\n", path);
     return;
 }
 
@@ -27,5 +85,40 @@ struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
 
     // YOUR CODE HERE
     //
-    return NULL;
+    strcpy(path, pathName);
+    if(strcmp(pathName, "/") == 0){
+        strcpy(baseName,pathName);
+        strcpy(dirName, "");
+        return root;
+    }
+    char* lastSlash = strrchr(pathName, '/');
+    char* targetName = malloc(100);
+    char* grandpaName = malloc(100);
+    if(lastSlash == NULL){
+        strcpy(baseName,pathName);
+        strcpy(dirName, "");
+        return cwd;
+    } else {
+        strcpy(baseName,lastSlash + 1);
+        *lastSlash = '\0';
+        baseName = strcpy(dirName, pathName);
+        lastSlash = strrchr(pathName, '/');
+        if(lastSlash == NULL){
+            strcpy(targetName, pathName);
+            strcpy(grandpaName, "/");
+        } else {
+            strcpy(targetName, lastSlash + 1);
+            *lastSlash = '\0';
+            lastSlash = strrchr(pathName, '/');
+            strcpy(grandpaName, pathName);
+            if(lastSlash != NULL) {
+                strcpy(grandpaName, lastSlash + 1);
+            }
+         
+    
+        }
+
+    }
+    strcpy(pathName, path);
+    return nodeSearch(root, targetName, grandpaName);
 }
